68864c4e3ba81d0b8756d7a6bbef9918d7154a29
lennert-mdware <hello@lennertderyck.be>
2021-08-06 22:15:47 +0200

    Update index.js

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -4,8 +4,9 @@ import Fade from 'react-reveal/Fade';
 const Modal = ({ open: initialState, children }) => {
     const [ open, setOpen ] = useState(initialState)
     
+    if (!open) return null
     return (
-        open && <div className="fixed top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center">
+        <div className="fixed top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center">
             <Fade bottom when={ open } collapse>
                 <div className="p-6 md:p-8 bg-white z-50">
                     { typeof children === 'function' ? children({ 
