e4b66e08a386b4f777bfcf3e9955b00393b91bc9
lennert-mdware <hello@lennertderyck.be>
2021-09-15 22:11:29 +0200

    Update requests.js

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import _keycl, { isLoggedIn, getToken, login, manualToken } from './keycloak.vendors';
+import _keycl, { isLoggedIn, getToken, login, initKeycloak } from './keycloak.vendors';
 
 const _axios = token => axios.create({
     baseURL: 'https://groepsadmin.scoutsengidsenvlaanderen.be/groepsadmin/rest-ga',
@@ -12,9 +12,9 @@ const _axios = token => axios.create({
 const base = (manualToken) => {
     // check if authenticated
     const authenticated = manualToken || isLoggedIn()
-    
+    console.log({ authenticated })
     if (!authenticated) {
-        login()
+        initKeycloak()
         return;
     }
     
