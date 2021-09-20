09e4c6cf4f2f43086bf6045ab0d44327ce119041
lennert-mdware <hello@lennertderyck.be>
2021-09-20 23:32:53 +0200

    Updated paymentpage

--- src/utils/index.js
+++ src/utils/index.js
@@ -44,10 +44,23 @@ export const findTag = (taglist) => {
     return taglist.filter(({ einde }) => !einde)
 }
 
-export const generatePaymentQR = ({ reciever = 'Groepskas', account = 'BE', amount = 0, descr = 'beetaling'}) => {
-    // const url = new URL(null, 'https://qrcode.tec-it.com/API/QRCode')
-    // console.log({ url });
+export const generatePaymentQR = ({ reciever = 'Groepskas', account = 'BE', amount = 0, descr = 'betaling'}) => {
     return `https://qrcode.tec-it.com/API/QRCode?data=BCD%0a001%0a1%0aSCT%0aKREDBEBB%0a${ reciever }+HAEGEPOORTERS%0a${ account }%0a${ amount }%0a%0a${ descr }&backcolor=%23ffffff&method=image`
+}
+export const generatePaymentCode = ({ reciever = 'grk', amount = 0, descr }) => {
+    const composedString = [reciever, amount, descr].join(';')
+    return btoa(composedString)
+}
+
+export const decodePaymentCode = (code) => {
+    if (!code) return;
+    
+    const [ reciever, amount, descr ] = atob(code).split(';')
+    return {
+        reciever,
+        amount,
+        descr
+    }
 }
 
 export const siteGroup = (value) => siteGroups.find(({ value: v }) => value === v);
