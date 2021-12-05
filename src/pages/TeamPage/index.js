784c02fd28053dbe4fb7f06fab55b2f80fdd31c1
Lennert De Ryck <hello@lennertderyck.be>
2021-12-05 23:13:29 +0100

    Update index.js

--- src/pages/TeamPage/index.js
+++ src/pages/TeamPage/index.js
@@ -1,19 +1,19 @@
 import React, { useContext, useState, createContext } from 'react';
 import { useQuery } from '@apollo/client';
 
-import { Button, Img, Icon, Modal } from '../../components';
+import { Button, Img, Icon, Modal, Container } from '../../components';
 import QUERIES from '../../graphql/queries';
 import PageLayout from '../../layouts/PageLayout';
 import { useVisitor } from '../../contexts/visitorContext';
 import { siteGroups } from '../../data/site';
-import { filterTeamOnFunction } from '../../utils';
+import { className, filterTeamOnFunction } from '../../utils';
 
 const teamContext = createContext();
 const { Provider } = teamContext;
 const useTeamContext = () => useContext(teamContext)
 
 const Card = ({ data }) => {
-    const { sensitiveHidden } = useVisitor()
+    const { sensitiveHidden, profile: { isLeader } } = useVisitor()
     const { content: { first_name, tel, image: { filename }, functions_extra, wel_name }} = data;
     const { toggleModal } = useTeamContext()
     
@@ -23,21 +23,30 @@ const Card = ({ data }) => {
     const handleSensitiveForceClick = () => {
         toggleModal()
     }
-    
+        
     const showTelByFunction = isGroupResp || isGrl;
+    const shown = ( sensitiveHidden && showTelByFunction );
     
     return <div className="border-b-2 border-gray-200 pb-6 h-full">
         <Img src={ filename } height="15rem" className="mb-4" />
         <h4 className="font-bold text-xl -mb-1">{ first_name }</h4>
         { wel_name && <p className="font-serif text-lg">{ wel_name }</p>}
-        {( sensitiveHidden && showTelByFunction) && <button onClick={ handleSensitiveForceClick } className="flex items-center mt-3">
+        
+        <button onClick={ !shown ? () => window.open('tel:' + tel, '_self') : handleSensitiveForceClick} className="flex items-center mt-3">
+            <Icon name="phone" size="1.2rem" className="mr-2" /> 
+            <span {...className(
+                (shown && !isLeader) && 'filter blur-sm'
+            )}>{ shown && !isLeader ? '+32412456789' : tel }</span>
+        </button>
+        
+        {/* {(( sensitiveHidden && showTelByFunction)) && <button onClick={ handleSensitiveForceClick } className="">
             <Icon name="phone" size="1.2rem" className="mr-2" /> 
             <span className="filter blur-sm">+32412456789</span>
         </button>}
         {( !sensitiveHidden && showTelByFunction) && <a href={ 'tel:' + tel } className="flex items-center mt-3">
             <Icon name="phone" size="1.2rem" className="mr-2" /> 
             <span>{ tel }</span>
-        </a>}
+        </a>} */}
     </div>
 }
 
@@ -58,7 +67,7 @@ const Group = ({ data: items }) => {
 
 const TeamPage = () => {
     const { data, loading } = useQuery(QUERIES.TEAM_FULL)
-    const { sensitiveHidden, showSensitive } = useVisitor()
+    const { sensitiveHidden, showSensitive, profile: { isLeader } } = useVisitor()
     const [ showModal, setShowModal ] = useState()
 
     const toggleModal = n => setShowModal(p => n ? n : !p)
@@ -88,6 +97,13 @@ const TeamPage = () => {
             </Modal>
             
             <PageLayout title="Leiding" subtitle="Ons team van gemotiveerde leiding" wide className="relative">
+                <Container>
+                    { isLeader && <div className="sticky bottom-0 p-4 bg-gray-200 mb-12 mt-6 flex items-center">
+                        <Icon name="information" size="1.3rem" className="mr-2" />
+                        Gsm-nummers worden weergegeven aangezien je aangemeld bent als leiding
+                    </div>}
+                </Container>
+                
                 <h3 className="font-serif mb-6 capitalize font-bold text-3xl">Groepsleiding</h3>
                 <div className="grid grid-cols-4 gap-6 mb-12">
                     { items.filter(({ content: { functions_extra }}) => functions_extra.includes('grl')).map(data => (
@@ -106,15 +122,7 @@ const TeamPage = () => {
                     </div>
                 ))}
                 
-                {/* <div className="grid grid-cols-4 gap-6">
-                    { items.map((data, index) => <div
-                        className="col-span-4 md:col-span-2 lg:col-span-1" 
-                        key={ index }
-                    >
-                        <Card data={ data } />
-                    </div>)}
-                </div> */}
-                { sensitiveHidden && <div className="sticky bottom-0 p-4 bg-gray-200 mx-8 mt-12">
+                {( sensitiveHidden && !isLeader) && <div className="sticky bottom-0 p-4 bg-gray-200 mx-8 mt-12">
                     <Button 
                         theme="simple" 
                         icon="eye-close"
