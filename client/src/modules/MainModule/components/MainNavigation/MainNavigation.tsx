f0daeacc0ecab3d6c844060e0931ff9fa5e1cd25
Lennert De Ryck <hello@lennertderyck.be>
2023-01-17 00:01:25 +0100

    Main navigation behaviour bugfix

--- client/src/modules/MainModule/components/MainNavigation/MainNavigation.tsx
+++ client/src/modules/MainModule/components/MainNavigation/MainNavigation.tsx
@@ -5,6 +5,7 @@ import classNames from 'classnames';
 import { NavLink } from 'react-router-dom';
 import NavItem from './NavItem';
 import { useDevice } from 'use-ua-parser-js';
+import { useMediaQuery } from 'react-responsive'
 
 interface Props {};
 
@@ -14,6 +15,9 @@ const MainNavigation: FC<Props> = () => {
     const isTouch = useMemo(() => {
         return device.type === 'mobile' || device.type === 'tablet';
     }, [device.type])
+    const isLargeScreen = useMediaQuery({
+        query: '(min-width: 1024px)'
+    })
     
     const menuItems = [
         { label: 'Startpagina', to: '/', icon: 'home-5' },
@@ -31,15 +35,15 @@ const MainNavigation: FC<Props> = () => {
     
     return (
         <>
-            <button onClick={ toggle } className="flex items-center whitespace-nowrap fixed top-4 right-4 z-50 w-12 h-12 bg-red-500 p-3 rounded-full shadow justify-center xl:hidden xl:hidden cursor-pointer">
+            <button onClick={ toggle } className="flex items-center whitespace-nowrap fixed top-4 right-4 z-50 w-12 h-12 bg-red-500 p-3 rounded-full shadow justify-center lg:hidden cursor-pointer">
                 <Icon name="menu" className="text-white" />
             </button>
-            <div className="xl:w-[68.5px]" />
+            <div className="lg:w-[68.5px]" />
             <aside 
                 className={classNames(
                     'fixed left-0 top-0 bottom-0 border-r-2 bg-white z-50',
                     'flex flex-col',
-                    active ? '-translate-x-0' : '-translate-x-full xl:-translate-x-0'
+                    active ? '-translate-x-0' : '-translate-x-full lg:-translate-x-0'
                 )} 
                 onMouseOver={() => open()} 
             >
@@ -49,7 +53,7 @@ const MainNavigation: FC<Props> = () => {
                     </button>
                     <NavLink to="/ga" className={({ isActive }) => classNames(
                         'overflow-hidden w-full h-full flex-1 flex justify-end bg-red-100',
-                        active ? 'opacity-100 max-w-[100vw]' : 'xl:opacity-0 xl:max-w-[0vw]',
+                        active ? 'opacity-100 max-w-[100vw]' : 'lg:opacity-0 lg:max-w-[0vw]',
                         isActive ? 'bg-opacity-100' : 'bg-opacity-0'
                     )}>
                         {({ isActive }) => (
@@ -69,7 +73,7 @@ const MainNavigation: FC<Props> = () => {
                 <nav className="flex-1 flex flex-col">
                     <div className="flex-1">
                         { menuItems.map((item, index) => (
-                            <NavItem onClick={() => isTouch && close()} key={ index } item={ item } active={ active } />
+                            <NavItem onClick={() => (isTouch || !isLargeScreen) && close()} key={ index } item={ item } active={ active } />
                         ))}
                     </div>
                     {/* <div className={classNames(
@@ -82,13 +86,14 @@ const MainNavigation: FC<Props> = () => {
                     </div> */}
                     <div>
                         { menuBottomItems.map((item, index) => (
-                            <NavItem onClick={() => isTouch && close()} key={ index } item={ item } active={ active } />
+                            <NavItem onClick={() => (isTouch || !isLargeScreen) && console.log('close()')} key={ index } item={ item } active={ active } />
                         ))}
                     </div>
                 </nav>
             </aside>
             <div 
-                onMouseOver={() => active && close()}
+                onMouseOver={() => active && isLargeScreen && close()}
+                onClick={() => close()}
                 className={classNames(
                     'fixed inset-0 bg-black z-40',
                     active ? 'bg-opacity-50 pointer-events-auto' : 'bg-opacity-0 pointer-events-none'
--- client/src/modules/MainModule/components/MainNavigation/NavItem.tsx
+++ client/src/modules/MainModule/components/MainNavigation/NavItem.tsx
@@ -9,14 +9,14 @@ interface Props {
     onClick?: () => void;
 };
 
-const NavItem: FC<Props> = ({ active, item, onClick }) => {
+const NavItem: FC<Props> = ({ active, item, onClick }) => {    
     return (
         <NavLink 
             onClick={() => onClick?.()}
             to={ item.to }
             className={({ isActive }) => classNames(
                 'flex items-center p-5 border-b-2 border-gray-200',
-                active ? 'border-opacity-100' : 'xl:border-opacity-0',
+                active ? 'border-opacity-100' : 'lg:border-opacity-0',
                 isActive && 'bg-red-100'
             )}
         >
@@ -27,7 +27,7 @@ const NavItem: FC<Props> = ({ active, item, onClick }) => {
                     )} />
                     <div className={classNames(
                         'overflow-hidden flex-1 flex items-center justify-between gap-28',
-                        active ? 'opacity-100 max-w-[100vw] pl-5' : 'xl:opacity-0 xl:max-w-[0vw] pl-5 xl:pl-0',
+                        active ? 'opacity-100 max-w-[100vw] pl-5' : 'lg:opacity-0 lg:max-w-[0vw] pl-5 lg:pl-0',
                         isActive ? 'text-red-500' : 'text-gray-600'
                     )}>
                         <span className="font-medium whitespace-nowrap">{ item.label }</span>
