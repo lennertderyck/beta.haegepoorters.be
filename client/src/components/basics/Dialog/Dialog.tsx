047f73ce1520a9c13f27352a9fb81f8ac25e00e4
Lennert De Ryck <hello@lennertderyck.be>
2023-05-02 22:55:51 +0200

    Update Dialog.tsx

--- client/src/components/basics/Dialog/Dialog.tsx
+++ client/src/components/basics/Dialog/Dialog.tsx
@@ -9,8 +9,8 @@ const Dialog: FC<Props> = ({ children, className, overlay, ...otherProps }) => {
     return (
         <div 
             className={classNames(
-                'fixed inset-0 z-40 xl:p-12 flex items-end justify-end bg-opacity-30',
-                overlay && 'xl:bg-black',
+                'fixed inset-0 z-40 xl:p-12 flex items-end justify-end',
+                overlay && 'bg-black bg-opacity-30 xl:bg-transparent',
                 className
             )}
             { ...otherProps }
