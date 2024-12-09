38531f90b881ae1f547da1eee8576b5a1d937069
Lennert De Ryck <lennyderyck@gmail.com>
2024-12-09 22:06:46 +0100

    HTTP header config

--- backend/api/server.ts
+++ backend/api/server.ts
@@ -16,28 +16,21 @@ const corsOpts: CorsOptions = {
 
     allowedHeaders: [
         'Content-Type',
+        'Authorization', 
+        'Access-Control-Allow-Origin',
     ],
 };
 
 const allowedContentTypes = ['application/json', 'image/png', 'image/jpg', 'image/jpeg', 'multipart/form-data']
 
-api.use((req, res, next) => {
-    // console.log(req)
-    next();
-})
-
-api.use(express.json()); // Gives error in body-parser package
-// api.use((req, res, next) => {
-//     const contentType = req.headers['content-type'] || 'text/plain';
-//     const contentTypeIsAllowed = allowedContentTypes.includes(contentType);
-    
-//     console.log('contentType', contentType)
-//     if (contentTypeIsAllowed) next();
-//     else res.status(400).send(`Content type ${contentType} not allowed. Allowed types: ${allowedContentTypes.join(', ')}`)
-// })
 api.use(cors(corsOpts));
+api.options('*', cors());
+api.use((req, res, next) => next())
+api.use(express.json()); // Gives error in body-parser package
 api.use(express.urlencoded({
-    extended: true
+    extended: true,
 }));
+
+
 
 export default api;
